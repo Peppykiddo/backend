d3ce79c9a4f67ea27ac017918e0c76d34cfd3176
Peppykiddo <tejaswinikalyani6@gmail.com>
2024-06-11 14:00:59 +0530

    change endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,7 +10,7 @@ const prisma = new PrismaClient();
 router.get("/getData", async (req, res) => {
     try {
       const data = await prisma.report.findMany();
-      res.status(60).json(data);
+      res.status(200).json(data);
     } catch (error) {
       console.error('Error fetching data:', error);
       res.status(500).json({ error: 'An error occurred while fetching data' });
@@ -79,7 +79,7 @@ router.post("/postData", async (req, res) => {
     });
   
     if(response){
-      res.status(200).json(response)
+      res.status(60).json(response)
     }else{
       res.status(500).json({msg:"internal server error"})
     }
